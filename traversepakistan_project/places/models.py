b58e34405dd646548d3ccb07657c491c42db4176
waqaraqeel <waqaraqeel@live.com>
2015-09-11 13:16:51 +0500

    places model done

--- traversepakistan_project/places/models.py
+++ traversepakistan_project/places/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from geoposition.fields import GeopositionField
+from taggit.managers import TaggableManager
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Place(models.Model):
+    category = models.ForeignKey(Category)
+    name = models.CharField(max_length=128, unique=True)
+    title = models.CharField(max_length=128)
+    coverImage = models.ImageField()
+    description = models.TextField()
+    gettingThere = models.TextField()
+    galleryPath = models.FilePathField()
+    location = GeopositionField()
+    tags = TaggableManager()
+
+    def __unicode__(self):
+        return self.name
--- traversepakistan_project/traversepakistan_project/settings.py
+++ traversepakistan_project/traversepakistan_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'places',
+    'geoposition',
+    'taggit',
 )
 
 MIDDLEWARE_CLASSES = (
