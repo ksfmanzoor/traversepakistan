cb0beb9f6287738096db7c35d1a387993f13c6c2
waqaraqeel <waqaraqeel@live.com>
2015-09-12 02:30:32 +0500

    removed separate home app

--- traversepakistan_project/traversepakistan_project/urls.py
+++ traversepakistan_project/traversepakistan_project/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.contrib import admin
+import views
 
 urlpatterns = patterns('',
     # Examples:
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('home.urls')),
+    url(r'^$', views.index, name='home'),
     url(r'^places/', include('places.urls')),
 )
